b103be0a04a71defeba4ea1bac93ff185d3d1e36
Paul Hernandez <paul.hernandez@opendeusto.es>
2024-05-21 18:18:13 +0200

    backend config dataBaseUrl updated

--- invoiceIt_backend/src/config/config.js
+++ invoiceIt_backend/src/config/config.js
@@ -2,6 +2,6 @@ require('dotenv').config();
 
 module.exports = {
     port: process.env.PORT || 5000,
-    databaseUrl: 'mongodb://localhost:27017/invoiceit'
+    databaseUrl: process.env.MONGO_URL
 };
 
